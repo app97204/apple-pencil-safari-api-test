279ae254c8b2b552a3eff5c398a58c82197bd455
YCY <chengyanyang0510@gmail.com>
2024-06-27 20:05:41 +0800

    prevent direct

--- index.js
+++ index.js
@@ -93,6 +93,7 @@ for (const ev of ['touchmove', 'mousemove']) {
   canvas.addEventListener(ev, function (e) {
     if (!isMousedown) return
     e.preventDefault()
+    if(touch.touchType === 'direct') return e.preventDefault()
 
     let pressure = 0.1
     let x, y
